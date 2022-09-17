a6409e38a4760a9813b29eea7c2546578e80e0ab
@sqeezelemon / Alexander Nikitin <64465635+sqeezelemon@users.noreply.github.com>
2022-09-18 01:04:22 +0300

    refactor: Change indentation + switch LAResponse.errorCode to LAError

--- Sources/SwiftyLiveApi/Types/LAError.swift
+++ Sources/SwiftyLiveApi/Types/LAError.swift
@@ -8,39 +8,46 @@ import Foundation
 
 /// An API error code.
 public enum LAError: Int, Error {
-    case ok = 0
-    case userNotFound = 1
-    case missingRequestParameters = 2
-    case endpointError = 3
-    case notAuthorized = 4
-    case serverNotFound = 5
-    case flightNotFound = 6
-    case noAtisAvailable = 7
-    /// An error code that is not present in the documentation, ideally should never appear.
-    case undocumented = 404
+  case ok = 0
+  case userNotFound = 1
+  case missingRequestParameters = 2
+  case endpointError = 3
+  case notAuthorized = 4
+  case serverNotFound = 5
+  case flightNotFound = 6
+  case noAtisAvailable = 7
+  /// Undocumented error code, please file a report on GitHub if encountered.
+  case undocumented
+}
+
+extension LAError: Decodable {
+  public init(from decoder: Decoder) throws {
+    let value = try decoder.singleValueContainer().decode(RawValue.self)
+    self = .init(rawValue: value) ?? .undocumented
+  }
 }
 
 extension LAError: LocalizedError {
-    public var errorDescription: String? {
-        switch self {
-        case .ok:
-            return "OK"
-        case .userNotFound:
-            return "User not found."
-        case .missingRequestParameters:
-            return "Missing request parameters."
-        case .endpointError:
-            return "Endpoint error."
-        case .notAuthorized:
-            return "Not authorized, check your API key."
-        case .serverNotFound:
-            return "Server not found."
-        case .flightNotFound:
-            return "Flight not found, perhaps it was ended."
-        case .noAtisAvailable:
-            return "ATIS is not active at this airport."
-        case .undocumented:
-            return "Unknown error."
-        }
+  public var errorDescription: String? {
+    switch self {
+    case .ok:
+      return "OK"
+    case .userNotFound:
+      return "User not found."
+    case .missingRequestParameters:
+      return "Missing request parameters."
+    case .endpointError:
+      return "Endpoint error."
+    case .notAuthorized:
+      return "Not authorized, check your API key."
+    case .serverNotFound:
+      return "Server not found."
+    case .flightNotFound:
+      return "Flight not found, perhaps it was ended."
+    case .noAtisAvailable:
+      return "ATIS is not active at this airport."
+    case .undocumented:
+      return "Unknown error, please file a report on GitHub."
     }
+  }
 }
--- Sources/SwiftyLiveApi/Types/LAResponse.swift
+++ Sources/SwiftyLiveApi/Types/LAResponse.swift
@@ -8,24 +8,19 @@ import Foundation
 
 /// Wrapper for all API responses.
 internal struct LAResponse<T: Decodable>: Decodable {
-    /// API error code.
-    var errorCode: Int
-    /// Response data
-    var result: T
-    
-    enum CodingKeys: CodingKey {
-        case errorCode, result
-    }
-    
-    init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        errorCode  = try values.decode(Int.self, forKey: .errorCode)
-        
-        guard errorCode == 0 else {
-            let apiError = LAError(rawValue: errorCode) ?? .undocumented
-            throw apiError
-        }
-        
-        result = try values.decode(T.self, forKey: .result)
-    }
+  /// API error code.
+  var errorCode: LAError
+  /// Response data.
+  var result: T
+  
+  private enum CodingKeys: CodingKey {
+    case errorCode, result
+  }
+  
+  init(from decoder: Decoder) throws {
+    let values = try decoder.container(keyedBy: CodingKeys.self)
+    errorCode  = try values.decode(LAError.self, forKey: .errorCode)
+    guard errorCode == .ok else { throw errorCode }
+    result = try values.decode(T.self, forKey: .result)
+  }
 }
