41126be034a144a652019c4ae85edc49cf4238d3
@sqeezelemon / Alexander Nikitin <64465635+sqeezelemon@users.noreply.github.com>
2022-09-18 18:48:44 +0300

    style: Change indentation

--- Sources/SwiftyLiveApi/Aircraft/LAAircraft.swift
+++ Sources/SwiftyLiveApi/Aircraft/LAAircraft.swift
@@ -8,13 +8,13 @@ import Foundation
 
 /// A single Infinite Flight aircraft.
 public struct LAAircraft: Decodable {
-  public init(id: String, name: String) {
-    self.id = id
-    self.name = name
-  }
-  
-  /// Unique ID of the aircraft.
-  public var id: String
-  /// Aircraft model name.
-  public var name: String
+    public init(id: String, name: String) {
+        self.id = id
+        self.name = name
+    }
+    
+    /// Unique ID of the aircraft.
+    public var id: String
+    /// Aircraft model name.
+    public var name: String
 }
--- Sources/SwiftyLiveApi/Types/LAResponse.swift
+++ Sources/SwiftyLiveApi/Types/LAResponse.swift
@@ -8,19 +8,19 @@ import Foundation
 
 /// Wrapper for all API responses.
 internal struct LAResponse<T: Decodable>: Decodable {
-  /// API error code.
-  var errorCode: LAError
-  /// Response data.
-  var result: T
-  
-  private enum CodingKeys: CodingKey {
-    case errorCode, result
-  }
-  
-  init(from decoder: Decoder) throws {
-    let values = try decoder.container(keyedBy: CodingKeys.self)
-    errorCode  = try values.decode(LAError.self, forKey: .errorCode)
-    guard errorCode == .ok else { throw errorCode }
-    result = try values.decode(T.self, forKey: .result)
-  }
+    /// API error code.
+    var errorCode: LAError
+    /// Response data.
+    var result: T
+    
+    private enum CodingKeys: CodingKey {
+        case errorCode, result
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        errorCode  = try values.decode(LAError.self, forKey: .errorCode)
+        guard errorCode == .ok else { throw errorCode }
+        result = try values.decode(T.self, forKey: .result)
+    }
 }
