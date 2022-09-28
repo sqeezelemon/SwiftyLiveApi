987165a966469aec54d715afd552daf51eac5bbd
@sqeezelemon / Alexander Nikitin <64465635+sqeezelemon@users.noreply.github.com>
2022-09-28 23:26:27 +0300

    feat: Make LAClientError conform to LocalizedError

--- Sources/SwiftyLiveApi/Utils/LAClientError.swift
+++ Sources/SwiftyLiveApi/Utils/LAClientError.swift
@@ -11,3 +11,16 @@ public enum LAClientError: Error {
     case urlIsNil
     case dateIsNil
 }
+
+extension LAClientError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .dataIsNil:
+            return "API Call didn't return any data."
+        case .urlIsNil:
+            return "Couldn't generate a URL for this request."
+        case .dateIsNil:
+            return "Couldn't parse Date."
+        }
+    }
+}
